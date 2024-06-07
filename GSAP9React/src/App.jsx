b43c93acae5c909dcc2411c8e4fc24f77bd3026c
psychic-coder <rohitganguly450@gmail.com>
2024-06-07 22:33:29 +0530

    react  gsap

---
+++ GSAP9React/src/App.jsx
@@ -0,0 +1,22 @@
+import React, { useRef, useState } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+//the way of writing the gsap
+
+const App = () => {
+
+
+
+
+  return (
+    <main>
+      <button
+      >
+        Animate
+      </button>
+      <div className="box"></div>
+    </main>
+  );
+};
+
+export default App;
